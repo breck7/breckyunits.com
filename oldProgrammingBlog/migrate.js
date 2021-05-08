9f4431aeef421fb23ca0b1037c84e66ea4abd934
Breck Yunits <breck7@gmail.com>
2021-05-07 15:19:12 -1000

    Fixed a bug in migrate.js that dropped the first sentence of most posts

--- oldProgrammingBlog/migrate.js
+++ oldProgrammingBlog/migrate.js
@@ -1,23 +1,24 @@
 // Imports a brecksblog php dump encoded to JSON to Scroll files.
 
-a = tree
-	.map((node) => {
-		const title = node.get("Title");
-		const timestamp = new Date(parseInt(node.getWord(0)) * 1000);
-		const permalink = jtree.Utils.stringToPermalink(title);
-		console.log(timestamp);
+a = Object.keys(posts)
+	.map((key) => {
+		const node = posts[key]
+		const title = node.Title
+		const timestamp = new Date(parseInt(key) * 1000)
+		const permalink = jtree.Utils.stringToPermalink(title)
+		console.log(timestamp)
 
 		//date 2012-12-18
-		const date = `${timestamp.getFullYear()}-${timestamp.getMonth()}-${timestamp.getDate()}`;
+		const date = `${timestamp.getFullYear()}-${timestamp.getMonth()}-${timestamp.getDate()}`
 
 		const data = `title ${title}
 date ${date}
 permalink ${permalink}
 
-${node.getNode("Essay").childrenToString()}`;
+${node.Essay}`
 
-		const tree = new TreeNode(`file ${permalink}.scroll`);
-		tree.nodeAt(0).appendLineAndChildren("data", data);
-		return tree.toString();
+		const tree = new TreeNode(`file ${permalink}.scroll`)
+		tree.nodeAt(0).appendLineAndChildren("data", data)
+		return tree.toString()
 	})
-	.join("\n");
+	.join("\n")
